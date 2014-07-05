924f8079223f5d5099c3bd8115c96dd69d1cb794
Ian <ian@Ians-MacBook-Air.local>
2014-07-05 16:01:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Oh My Py
+
+As it turns out, [ipython](http://ipython.org/) is actually really good at being a command shell, though it needs a little bit of tuning out of the box to really be usable as a day-to-day replacement for `fish`, `zsh`, et al.
+
+`ohmypy` aims to address the tuning part of that by overriding some of IPython's default behavior and adding more shell-like functionality.
+
+Right now it's pretty small, not extensively tested, and based more on my own preferences than anything else. In its current work-in-progress state I'd consider it a decent jumping-off point to learn how to build the same kind of thing yourself, but it's not exactly production ready.
+
+
+## Install
+
+    $ git clone https://github.com/ianpreston/ohmypy.git
+    $ ipython profile create sh
+    $ ln -s /path/to/ohmypy/extension.py ~/.ipython/extensions/ohmypy.py
+    $ ln -s /path/to/ohmypy/config.py ~/.ipython/profile_sh/ipython_config.py
+
+
+## License
+
+The MIT License (MIT)
+
+Copyright (C) 2014 Ian Preston
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ extension.py
@@ -0,0 +1,173 @@
+import subprocess
+import shlex
+import os
+import os.path
+import keyword
+
+from IPython.core.inputtransformer import StatelessInputTransformer
+from IPython.utils.text import SList
+
+
+def bltn_dot_slash(line, tokens):
+    if line.startswith('.'):
+        _execute(line)
+        return True
+
+    return False
+
+
+def bltn_cd(line, tokens):
+    if tokens[0] == 'cd':
+        path_spec = ' '.join(tokens[1:])
+        path_spec = os.path.expanduser(path_spec)
+        path_spec = ipython.var_expand(path_spec)
+        os.chdir(path_spec)
+        return True
+
+    return False
+
+
+BUILTINS = [
+    # TODO - Builtin equivelant for 'workon'
+    bltn_dot_slash,
+    bltn_cd,
+]
+
+ALIAS_EXEMPT = keyword.kwlist + [
+    'cd',
+    'exit',
+    'env',
+]
+
+ipython = None
+aliases = {}
+
+
+def _initialize_aliases():
+    global aliases
+
+    # FIXME - Assumes $PATH is delimited by ':'
+    roots = os.environ['PATH'].split(':')
+    roots = [r.strip() for r in roots if r.strip()]
+
+    for root in roots:
+        for child in os.listdir(root):
+            child_abs  = os.path.join(root, child)
+            child_base = os.path.basename(child_abs)
+            if child_base in ALIAS_EXEMPT:
+                continue
+            if os.access(child_abs, os.X_OK):
+                aliases[child_base] = child_abs
+
+
+class ShellResult(str):
+    @classmethod
+    def make(cls, stdout, stderr, code, cmd):
+        inst = cls(stdout)
+        inst.stdout = stdout
+        inst.stderr = stderr
+        inst.code = code
+        inst.cmd = cmd
+        return inst
+
+    def __gt__(self, filename):
+        with open(filename, 'w') as f:
+            f.write(self)
+
+    def __rshift__(self, filename):
+        with open(filename, 'a') as f:
+            f.write(self)
+
+    @property
+    def l(self):
+        return self.splitlines()
+
+
+def _shell_out(command):
+    command = ipython.var_expand(command)
+    p = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, stderr = p.communicate()
+    code = p.wait()
+
+    return ShellResult.make(stdout=stdout, stderr=stderr, code=code, cmd=command)
+
+
+def _shell_out_tty(command):
+    command = ipython.var_expand(command)
+    os.system(command)
+
+
+def _execute(command):
+    output = _shell_out(command)
+
+    # Create (or overwrite) a local variable in the user's namespace
+    # called '_', and assign its value as the result of the shell command.
+    # Display the command's stdout as well, as is expected of command shells
+    print output,
+    ipython.push({'_': output})
+    return output
+
+
+@StatelessInputTransformer.wrap
+def bang(line):
+    if '!' not in line:
+        return line
+
+    shell_start = line.index('!')
+    command = line[shell_start+1:]
+    command = ipython.var_expand(command)
+
+    # Execute the given command, injecting a new variable into the user's
+    # namespace, then replace the shell command portion of the input line
+    # with the new variable's identifier. End the line with a semicolon
+    # so that repr(output) won't be displayed to the user.
+    output = _execute(command)
+    return line[:shell_start] + '_' + ';'
+
+
+@StatelessInputTransformer.wrap
+def alias(line):
+    if not len(line):
+        return ''
+
+    tokens = shlex.split(line.encode('utf8'))
+    command = tokens[0]
+
+    if command not in aliases.keys():
+        return line
+
+    resolved_command = aliases[command]
+    shell_line = [resolved_command] + tokens[1:]
+    shell_line = ' '.join(shell_line)
+
+    # TODO - In many cases, _execute would be a better option here than
+    _shell_out_tty(shell_line)
+    return ''
+
+
+@StatelessInputTransformer.wrap
+def builtin(line):
+    tokens = shlex.split(line.encode('utf8'))
+
+    for bltn in BUILTINS:
+        activated = bltn(line, tokens)
+        if activated:
+            return ''
+
+    return line
+
+
+def load_ipython_extension(_ipython):
+    global ipython
+    ipython = _ipython
+
+    ipython.input_transformer_manager.logical_line_transforms.insert(0, bang())
+    ipython.input_transformer_manager.logical_line_transforms.insert(1, builtin()) 
+    ipython.input_transformer_manager.logical_line_transforms.insert(2, alias())
+
+    _initialize_aliases()
