bb59d7e4e9adecad6c43771904002ee048062b9b
Ian Preston <ian@ian-preston.com>
2014-07-27 13:23:05 -0700

    builtin module as a plugin

--- extension/__init__.py
+++ extension/__init__.py
@@ -3,13 +3,14 @@ import os.path
 import sys
 
 from plugin.alias import AliasPlugin
+from plugin.builtin import BuiltinPlugin
 from path import initialize_path
 
 
 def load_ipython_extension(ipython):
     initialize_path()
 
-    for pl in (AliasPlugin,):
+    for pl in (AliasPlugin, BuiltinPlugin):
         plinst = pl(ipython)
         plinst.load()
 
