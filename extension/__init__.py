8d38bd67b96c0f8e61e6c5bb627face04cf73797
Ian Preston <ian@ian-preston.com>
2014-08-03 14:22:20 -0700

    virtualenv support via %activate magic

--- extension/__init__.py
+++ extension/__init__.py
@@ -5,6 +5,7 @@ import sys
 from plugin.alias import AliasPlugin
 from plugin.builtin import BuiltinPlugin
 from plugin.shell import ShellPlugin
+import magic.activate
 from path import initialize_path
 
 
@@ -15,3 +16,5 @@ def load_ipython_extension(ipython):
         plinst = pl(ipython)
         plinst.load()
 
+    for ml in (magic.activate,):
+        ml.load(ipython)
